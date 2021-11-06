b9c236103fcd865bfa983f55c210de8cd5778312
t6-thu <niuhy18@mails.tsinghua.edu.cn>
2021-11-06 22:51:56 +0800

    record s,a,r,s_

--- scripts/record_offlinerl_dataset.py
+++ scripts/record_offlinerl_dataset.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import random
 
 from wrapped_carla_env import create_wrapped_carla_single_car_env
+from car.reward import CarReward
 
 import carla
 from tqdm import tqdm
@@ -21,43 +22,55 @@ def record_dataset(
     # set gpu index
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_index)
 
-    # step env
-    env = create_wrapped_carla_single_car_env(global_config={}, gpu_index=gpu_index)
+    global_config = {}
+    # TODO: when recording real dataset, fix the map and ensure its lifetime to be enough
+    if save_path != "./dataset/sim":
+        global_config = {"map_list" : ['Town01'], "map_lifetime" : n_steps}
+    
+    # TODO: create an env for single car
+    env = create_wrapped_carla_single_car_env(global_config=global_config, gpu_index=gpu_index)
     obs = env.reset()
     for step in tqdm(range(n_steps)):
         if random.random() < eps:
             # eps-greedy act
             act = env.action_space.sample() * rand_action_range
 
+            # TODO: Close Autopilot and choose action randomly
             env.unwrapped.server_manager.client.apply_batch_sync([
                 carla.command.SetAutopilot(env.unwrapped.car_manager.cars[0].actor.id, False, env.unwrapped.server_manager.tm_port)
             ])
             next_obs, rew, done, info = env.step([act])
+            # TODO: Open Autopilot anew
             env.unwrapped.server_manager.client.apply_batch_sync([
                 carla.command.SetAutopilot(env.unwrapped.car_manager.cars[0].actor.id, True, env.unwrapped.server_manager.tm_port)
             ])
         else:
             next_obs, rew, done, info = env.step([None])
-            # get act
+            # TODO: get action and reward from Autopilot
             car_control = env.unwrapped.car_manager.cars[0].actor.get_control()
             act = [car_control.throttle - car_control.brake, car_control.steer]
+            rew, _ = env.unwrapped.car_manager.cars[0].get_reward_done()
 
-            # save obs & act
+            # save observation(jpg) & action & reward
             obs_cv = cv2.cvtColor(obs.transpose((1, 2, 0)), cv2.COLOR_RGB2BGR)
-            cv2.imwrite(os.path.join(save_path, "{}.jpg".format(step)), obs_cv)
-            with open(os.path.join(save_path, "{}.json".format(step)), "wt") as f:
-                json.dump({"act": act}, f)
+            cv2.imwrite(os.path.join(save_path, "s{}.jpg".format(step)), obs_cv)
+            with open(os.path.join(save_path, "a{}-r{}.json".format(step, step)), "wt") as f:
+                json.dump({"action": act, "reward": rew}, f)
                 f.close()
 
         # next & done reset
         obs = next_obs
+        # save next observation(jpg)
+        next_obs_cv = cv2.cvtColor(obs.transpose((1, 2, 0)), cv2.COLOR_RGB2BGR)
+        cv2.imwrite(os.path.join(save_path, "s{}_.jpg".format(step)), next_obs_cv)
         if done:
             obs = env.reset()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("save_path", type=str, help="Path to save dataset")
+    # TODO: path for dataset recording
+    parser.add_argument("save_path", type=str, default="sim", help="Path to save dataset")
     parser.add_argument("--n", type=int, default=int(2e5), help="Number of steps to collect")
     parser.add_argument("--n_jobs", type=int, default=10, help="Number of worker processes")
     parser.add_argument("--devices", type=str, default="0,1", help="GPUs to use")
@@ -71,7 +84,7 @@ def main():
 
     processes = []
     for job_id in range(n_jobs):
-        save_path = os.path.join(args.save_path, str(job_id))
+        save_path = os.path.join("./dataset", args.save_path, str(job_id))
         os.makedirs(save_path, exist_ok=True)
 
         gpu_index = devices[job_id % len(devices)]
